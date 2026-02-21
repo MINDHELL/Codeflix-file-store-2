42b339565bfd683be8df8b735206c6b62489c136
MINDHELL <Tgpurpose640@gmail.com>
2026-02-21 17:00:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,9 +40,9 @@ START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ec17880d61180d3
 FORCE_PIC = os.environ.get("FORCE_PIC", "https://telegra.ph/file/e292b12890b8b4b9dcbd1.jpg")
 
 #--------------------------------------------
-SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "linkshortify.com")
-SHORTLINK_API = os.environ.get("SHORTLINK_API", "")
-TUT_VID = os.environ.get("TUT_VID","https://t.me/hwdownload/3")
+SHORTLINK_URL = os.environ.get("SHORTLINK_URL", "Vplink.in")
+SHORTLINK_API = os.environ.get("SHORTLINK_API", "ee4c6892fd0acac564be8afb9a2db6b7ddbac61e"")
+TUT_VID = os.environ.get("TUT_VID","https://t.me/allvidsbackup3/3")
 SHORT_MSG = "<b>⌯ Here is Your Download Link, Must Watch Tutorial Before Clicking On Download...</b>"
 
 SHORTENER_PIC = os.environ.get("SHORTENER_PIC", "https://telegra.ph/file/ec17880d61180d3312d6a.jpg")
