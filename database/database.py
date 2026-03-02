8d04dff72b58e9cb39d336d1d99ad01999b444ed
MINDHELL <Tgpurpose640@gmail.com>
2026-03-02 20:20:06 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -48,6 +48,9 @@ class Rohit:
         self.fsub_data = self.database['fsub']   
         self.rqst_fsub_data = self.database['request_forcesub']
         self.rqst_fsub_Channel_data = self.database['request_forcesub_channel']
+        self.special_links = self.database['special_links']
+
+    
         
 
 
@@ -68,6 +71,23 @@ class Rohit:
     async def del_user(self, user_id: int):
         await self.user_data.delete_one({'_id': user_id})
         return
+        
+# Create special link
+async def create_special_link(self, original_token: str, mode: str):
+    new_token = secrets.token_urlsafe(16)
+
+    await self.special_links.insert_one({
+        "_id": new_token,
+        "original_token": original_token,
+        "mode": mode
+    })
+
+    return new_token
+
+
+# Get special link
+async def get_special_link(self, token: str):
+    return await self.special_links.find_one({"_id": token})
 
 
     # ADMIN DATA
