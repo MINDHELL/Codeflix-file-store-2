29f62fbdef838de31f59af4584922445fac51d87
MINDHELL <Tgpurpose640@gmail.com>
2026-03-03 21:02:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -45,7 +45,7 @@ async def check_owner(client, message):
 SHORTENERS = [
     {"url": "vplink.in", "api": "ee4c6892fd0acac564be8afb9a2db6b7ddbac61e"},
     {"url": "get2short.com", "api": "7eed58ce58ac8b850e310f1f10903421388c3d36"},
-    {"url": "https://anotherlink.in/api", "api": "API_KEY_3"},
+    {"url": "gplinks.com", "api": "b3dcabdd7bb07a93c8135996ae1b79cb2ab30566"},
 ]
 
 
